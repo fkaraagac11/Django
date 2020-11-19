be5d3d50d19aa8575221c452122651b490a27f79
fkaraagac11 <fehmi11.qa@gmail.com>
2020-11-18 20:39:41 -0500

    update

--- felipe/calc/urls.py
+++ felipe/calc/urls.py
@@ -4,3 +4,11 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
 ]
+
+
+# urlpatterns = [
+#     path('articles/2003/', views.special_case_2003),
+#     path('articles/<int:year>/', views.year_archive),
+#     path('articles/<int:year>/<int:month>/', views.month_archive),
+#     path('articles/<int:year>/<int:month>/<slug:slug>/', views.article_detail),
+# ]
